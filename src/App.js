ea9ae5afa5364e0b614a38a41fc62856852b1eba
tapiocaPENGUIN <elijah.brunsvold@gmail.com>
2019-02-19 12:37:05 -0600

    Hello World

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Landing from './components/Landing'
 
 class App extends Component {
   render() {
     return (
-      <div>Hello World </div>
-      
+    
+
+      <Landing />
+
     );
   }
 }
