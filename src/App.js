0a217f994b27e92023a0cc817508d9ca4291dc98
tapiocaPENGUIN <elijah.brunsvold@gmail.com>
2019-02-20 01:23:33 -0600

    Fixed issue in App.js with importing footer but not using it

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Landing from './components/Landing'
 import InteractiveResume from './components/InteractiveResume';
-import Footer from './components/Footer';
+
 import './index.css';
 
 
