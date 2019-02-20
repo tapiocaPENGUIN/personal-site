9de43bdd30cb1400d4a3b5b01434b3b977946615
tapiocaPENGUIN <elijah.brunsvold@gmail.com>
2019-02-20 01:11:24 -0600

    Added a bunch to the resume section

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 import Landing from './components/Landing'
 import InteractiveResume from './components/InteractiveResume';
+import Footer from './components/Footer';
 import './index.css';
+
 
 class App extends Component {
   render() {
     return (
     <div>
     <Landing />
+
     <InteractiveResume />
+
     </div>
+
+
 
     );
   }
--- src/components/InteractiveResume.js
+++ src/components/InteractiveResume.js
@@ -5,8 +5,53 @@ import '../stylesheets/splash.css';
 class InteractiveResume extends React.Component {
     render() {
         return(
-            
-            <div>Resume Goes here</div>
+            <div className="Container">
+                <div className="Tech-Title">
+                    <p>Techologies</p>
+                </div>
+                <div className="Tech-Info">
+                    <ul>
+
+                        <li>React</li>
+                        <li>HTML/CSS</li>
+                        <li>Java</li>
+                        <li>SQL</li>
+                        <li>Google Firebase/FireStore</li>
+
+                    </ul>
+                </div>
+
+                <div className="Job-Title">
+                    <p>Work Experience</p>
+                </div>
+                <div className="Job-Info">
+                    <h5>CraneIT, LLC</h5>
+                    <h6>IT Support Assistant, 2016 - Present </h6>
+                    <p>I help provide a wide variety of IT support to a broad selection of clients.  Whether it is in the offices of accounting firms, or out on the links of a golf course fixing/installing
+                        workstations, POS machines, VOIP phones, servers, and wireless access points.
+                    </p>
+
+                    <h5>St. Charles County Youth Soccer Association</h5>
+                    <h6>Referee, 2011 - Present </h6>
+                    <p>
+
+                        As a referee my duties include reviewing and mentoring new referees, defusing conflict with players, coaches, and fans, and managing volative situations efficiently and diplomatically.
+                    </p>
+                </div>
+                <div className="Edu-Title">
+                    <p>Education</p>
+                </div>
+                <div className="Edu-Info">
+                    <h5>University of Missouri - St. Louis</h5>
+                    <h6><strong><u>Bachelor of Science in Information Systems</u></strong> December 2019 </h6>
+                    <h6>Expected Graduation, December 2019 </h6>
+
+                    <h5>St. Charles Community College</h5>
+                    <h6><strong><u>Associate of Applied Science Computer Science - Cyber Security</u></strong></h6>
+                    <h6>Graduated, May 2017</h6>
+
+                </div>
+            </div>
         );
     }
 }
