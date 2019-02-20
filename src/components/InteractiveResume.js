3846ca23875dd34f03f9645e7a04bd76f62e41f1
tapiocaPENGUIN <elijah.brunsvold@gmail.com>
2019-02-19 19:38:00 -0600

    fixed error

---
+++ src/components/InteractiveResume.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../stylesheets/splash.css';
+
+
+class InteractiveResume extends React.Component {
+    render() {
+        return(
+            
+            <div>Resume Goes here</div>
+        );
+    }
+}
+
+
+export default InteractiveResume
