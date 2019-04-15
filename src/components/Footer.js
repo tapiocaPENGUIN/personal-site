cbd7fc1d5b6684526232710e4f978839c69fdfb3
tapiocaPENGUIN <elijah.brunsvold@gmail.com>
2019-04-14 20:49:46 -0500

    Added more stuff to projects

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../stylesheets/splash.css';
+
+
+class Footer extends React.Component {
+    render() {
+        return(
+
+            <div className="splash">
+                <h1>footer</h1>
+            </div>
+        );
+    }
+}
+
+
+export default Footer
--- src/components/InteractiveResume.js
+++ src/components/InteractiveResume.js
@@ -6,6 +6,32 @@ class InteractiveResume extends React.Component {
     render() {
         return(
             <div className="Container">
+                <div className="Job-Title">
+                    <p>Coding Projects</p>
+                </div>
+                <div className="Job-Info">
+                    <h4>Java Projects</h4>
+                    <h6><strong><u>2019 UMSL | Hack</u></strong></h6>
+                    <p>Community service benefits app:  This app was built at an UMSL 36-hour hackathon using Java and Android Studio
+                        Its purpose was to increase local volunteering by showing users the local opportunities to volunteer.
+                        It also assigned points for volunteering which users could redeem for benefits.  The user accounts, with the point totals, were stored in Google FireStore.  We used Github for
+                        version control.
+                    </p>
+                    <h6><strong><u>Coffee Rating App</u></strong></h6>
+                    <p>This Android app let users rate their favorite StarBucks coffees.  It was coded using Android Studio, and the ratings and drinks where stored in Google FireBase.
+                    </p>
+                    <h6><strong><u>ATM Project</u></strong></h6>
+                    <p>This project had similar functions to an ATM or bank; it allowed the users to create different accounts-either checking or savings.
+                        The accounts where creating using Java inheritence and stored in the Derby Client Database.
+                    </p>
+                    <h6><strong><u>Employee payment software</u></strong></h6>
+                    <p>Employee payment software:  This project allowed the user to create new employee profiles and make them either hourly or salary based.
+                        Then it could determine how much they would be paid in a period.
+                    </p>
+
+
+                </div>
+
                 <div className="Tech-Title">
                     <p>Techologies</p>
                 </div>
@@ -27,13 +53,13 @@ class InteractiveResume extends React.Component {
                     <p>Work Experience</p>
                 </div>
                 <div className="Job-Info">
-                    <h5>CraneIT, LLC</h5>
+                    <h4>CraneIT, LLC</h4>
                     <h6><strong><u>IT Support Assistant</u></strong>, 2016 - Present </h6>
                     <p>I help provide a wide variety of IT support to a broad selection of clients.  Whether it is in the offices of accounting firms, or out on the links of a golf course fixing/installing
                         workstations, POS machines, VOIP phones, servers, and wireless access points.
                     </p>
 
-                    <h5>St. Charles County Youth Soccer Association</h5>
+                    <h4>St. Charles County Youth Soccer Association</h4>
                     <h6><strong><u>Referee</u></strong>, 2011 - Present </h6>
                     <p>
 
@@ -44,11 +70,11 @@ class InteractiveResume extends React.Component {
                     <p>Education</p>
                 </div>
                 <div className="Edu-Info">
-                    <h5>University of Missouri - St. Louis</h5>
+                    <h4>University of Missouri - St. Louis</h4>
                     <h6><strong><u>Bachelor of Science in Information Systems</u></strong> December 2019 </h6>
                     <h6>Expected Graduation, December 2019 </h6>
 
-                    <h5>St. Charles Community College</h5>
+                    <h4>St. Charles Community College</h4>
                     <h6><strong><u>Associate of Applied Science Computer Science - Cyber Security</u></strong></h6>
                     <h6>Graduated, May 2017</h6>
 
